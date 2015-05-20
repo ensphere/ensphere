a695f3071f5a5d1f2c7d721fae2f2dbe0094efb2
Luke Snowden <luke@purposemedia.co.uk>
2015-05-20 23:00:48 +0100

    update

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,7 +37,7 @@ class RouteServiceProvider extends ServiceProvider {
 	{
 		$router->group(['namespace' => $this->namespace], function($router)
 		{
-			require __DIR__ . '/../Http/routes.php');
+			require __DIR__ . '/../Http/routes.php';
 		});
 	}
 
