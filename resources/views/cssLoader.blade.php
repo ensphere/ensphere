d75912b8ba817bf5dd5ecfb7d1cf242766a4fc6a
Luke Snowden <luke@purposemedia.co.uk>
2015-05-20 08:42:12 +0100

    update

--- resources/views/cssLoader.blade.php
+++
@@ -1,5 +0,0 @@
-<link href="/vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-	<link href="/vendor/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
-	<link href="/vendor/alertify.js/themes/alertify.core.css" rel="stylesheet">
-	<link href="/vendor/alertify.js/themes/alertify.default.css" rel="stylesheet">
-	
--- resources/views/jsLoader.blade.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript" src="/vendor/jquery/dist/jquery.min.js"></script>
-	<script type="text/javascript" src="/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="/vendor/alertify.js/lib/alertify.min.js"></script>
-	
