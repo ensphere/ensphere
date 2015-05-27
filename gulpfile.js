bfc740cfec7353f93a77cbc9b5e25e9e49f1c5fb
Luke Snowden <luke@purposemedia.co.uk>
2015-05-27 22:45:07 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -17,11 +17,10 @@ elixir(function(mix) {
     .sass("ensphere.scss", "public/package/ensphere/ensphere/css")
     .copy("resources/assets/images/", "public/package/ensphere/ensphere/images/")
 	.copy("resources/assets/js/", "public/package/ensphere/ensphere/js/")
-	.copy("assets.json", "public/package/ensphere/ensphere/assets.json")
 
 	// Dependencies
-	.copy("vendor/bower_components/bootstrap", "public/vendor/bootstrap")
-	.copy("vendor/bower_components/jquery", "public/vendor/jquery")
+	.copy("bower_components/bootstrap", "public/vendor/bootstrap")
+	.copy("bower_components/jquery", "public/vendor/jquery")
 
     // Application Based Assets
     .stylesIn("public/package/", "public/css/packages.all.css")
