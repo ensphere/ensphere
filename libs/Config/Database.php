cac9f9f7a6018e93cc67655a649ae092c3b70eca
Connor Burton <connorburton@Connors-iMac.local>
2016-02-17 15:51:16 +0000

    Retain unix_socket option on all envs

--- libs/Config/Database.php
+++ libs/Config/Database.php
@@ -5,16 +5,17 @@ use App;
 class Database {
 
 	/**
-	 * If on local environment, DB_SOCKET is an available .env option, if we can find a MAMP socket, that's the default
+	 * If on local environment, if we can find a MAMP socket, that's the default db socket
 	 * @param  array  $array [description]
 	 * @return [type]        [description]
 	 */
 	public static function mySQLconnection( $array ) {
+		$mampSocket = '';
 		if ( env( 'APP_ENV' ) === 'local' ) {
  			$path = '/Applications/MAMP/tmp/mysql/mysql.sock';
- 			$mampSocket = ( file_exists( $path ) ) ? $path : '';
- 			$array['unix_socket'] = env( 'DB_SOCKET', $mampSocket );
+ 			if( file_exists( $path ) ) $mampSocket = $path;
   		}
+  		$array['unix_socket'] = env( 'DB_SOCKET', $mampSocket );
 		return $array;
 	}
 
