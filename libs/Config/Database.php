45a8ab9fdb779599e40484c08cda3b0fa2e887b0
Connor Burton <connorjburton@gmail.com>
2016-02-17 13:21:07 +0000

    Default socket for MAMP environments

--- libs/Config/Database.php
+++ libs/Config/Database.php
@@ -5,13 +5,16 @@ use App;
 class Database {
 
 	/**
-	 * [mySQLconnecttion description]
-	 * @param  array  $array [description]
-	 * @return [type]        [description]
+	 * If on local environment, DB_SOCKET is an available .env option, if we can find a MAMP socket, that's the default
+	 * @param  array  $array
+	 * @return array  $array
 	 */
 	public static function mySQLconnection( $array ) {
-		if ( env( 'APP_ENV' ) === 'local' && ! is_null( $socket = env( 'DB_SOCKET' ) ) ) {
-			$array['socket'] = $socket;
+		if ( env( 'APP_ENV' ) === 'local' ) {
+			$path = '/Applications/MAMP/tmp/mysql/mysql.sock';
+			$mampSocket = ( file_exists( $path ) ) ? $path : '';
+			
+			$array['unix_socket'] = env( 'DB_SOCKET', $mampSocket );
 		}
 		return $array;
 	}
