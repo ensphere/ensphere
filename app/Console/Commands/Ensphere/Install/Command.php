7a2af017480907f2b136d28154a78e0a2b956669
Luke Snowden <luke@purposemedia.co.uk>
2016-02-18 21:59:02 +0000

    removing npm from install

--- app/Console/Commands/Ensphere/Install/Command.php
+++ app/Console/Commands/Ensphere/Install/Command.php
@@ -36,12 +36,12 @@ class Command extends IlluminateCommand {
 	public function fire()
 	{
 		if( ! $this->hasBeenInstalled() ) {
-			$this->installNodeModules();
+			//$this->installNodeModules();
 			$this->installBowerComponents();
 			$this->generateRegistrationFile();
 			$this->publishVendorAssets();
 			$this->combineVendorAssets();
-			$this->runGulp();
+			//$this->runGulp();
 			$this->defineAsinstalled();
 		} else {
 			$this->error( "application has already ran the install process --canceled" );
