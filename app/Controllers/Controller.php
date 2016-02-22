3d4acc3eaff5d84f1abe52574260ca9531993958
Luke Snowden <luke@purposemedia.co.uk>
2016-02-22 21:23:17 +0000

    update namespace

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ensphere\Authentication\Http\Controllers;
+namespace Ensphere\Ensphere\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
