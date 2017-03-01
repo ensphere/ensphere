dcf4a7ef357931d4e02d925dcfc3a121e6349238
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-03-01 08:30:35 +0000

    added global handler to ensphere

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,81 +1,29 @@
 <?php
 
-namespace Ensphere\Ensphere\Exceptions;
+namespace Purposemedia\AdminMediaManager\Exceptions;
 
+use EnsphereCore\Libs\Exceptions\ExceptionHandler;
 use Exception;
-use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Illuminate\Foundation\Validation\ValidationException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\Exception\HttpResponseException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        AuthorizationException::class,
-        HttpException::class,
-        ModelNotFoundException::class,
-        ValidationException::class,
-        HttpResponseException::class
-    ];
 
     /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $e
-     * @return void
+     * @param Exception $e
      */
-    public function report(Exception $e)
+    public function report( Exception $e )
     {
-        return parent::report($e);
+        parent::report( $e );
     }
 
     /**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $e
-	 * @return \Illuminate\Http\Response
-	 */
-	public function render($request, Exception $e)
-	{
-		if ( $this->isHttpException($e) )
-        {
-            return $this->renderHttpException($e);
-        }
-        if ( config('app.debug') )
-        {
-        	if( ! in_array( get_class( $e ), $this->dontReport ) ) {
-            	return $this->renderExceptionWithWhoops($e);
-            }
-        }
-		return parent::render($request, $e);
-	}
-
-	/**
-     * Render an exception using Whoops.
-     *
-     * @param  \Exception $e
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param Exception $e
+     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\Response
      */
-    protected function renderExceptionWithWhoops(Exception $e)
+    public function render( $request, Exception $e )
     {
-        $whoops = new \Whoops\Run;
-        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
-
-        return new \Illuminate\Http\Response(
-            $whoops->handleException($e),
-            $e->getStatusCode(),
-            $e->getHeaders()
-        );
+        return parent::render( $request, $e );
     }
+
 }
