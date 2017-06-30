45cad05ad8372a12911dc28142e671ff44e4a833
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-06-30 09:48:07 +0100

    updated lock file for quicker install, updated dev stuff

---
+++ resources/assets/src/js/ensphere.js
@@ -0,0 +1,12 @@
+/* Definition of handler based on device
+ ———————————————————————————————————————— */
+var eventClickHandler = 'click';
+if ( 'ontouchstart' in document.documentElement ) {
+    clickHandler = 'eventClickHandler';
+}
+
+/* Global calls
+ ———————————————————————————————————————— */
+$(function () {
+
+});
