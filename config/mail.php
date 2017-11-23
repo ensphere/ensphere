6f8c144be1b063c68f5f24aef959912ba7ae1b0d
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-11-23 12:52:28 +0000

    update to config

--- config/mail.php
+++ config/mail.php
@@ -15,7 +15,7 @@ return [
 	|
 	*/
 
-	'driver' => env('MAIL_DRIVER', 'smtp'),
+	'driver' => env( 'MAIL_DRIVER', 'smtp' ),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -28,7 +28,7 @@ return [
 	|
 	*/
 
-	'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+	'host' => env( 'MAIL_HOST', 'smtp.mailgun.org' ),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return [
 	|
 	*/
 
-	'port' => env('MAIL_PORT', 587),
+	'port' => env( 'MAIL_PORT', 587 ),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,10 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => [
+	    'address' => env( 'MAIL_FROM_ADDRESS', null ),
+        'name' => env( 'MAIL_FROM_NAME', null ),
+    ],
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +83,7 @@ return [
 	|
 	*/
 
-	'username' => env('MAIL_USERNAME'),
+	'username' => env( 'MAIL_USERNAME' ),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +96,7 @@ return [
 	|
 	*/
 
-	'password' => env('MAIL_PASSWORD'),
+	'password' => env( 'MAIL_PASSWORD' ),
 
 	/*
 	|--------------------------------------------------------------------------
