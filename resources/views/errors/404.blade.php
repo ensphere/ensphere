5ad8199553b31ed84d0a06522f0b180363d80d03
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-12-11 10:37:22 +0000

    incorrect id on loader

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
 	</head>
 	<body class="error-404">
-		<div id="screen-loader" style="position: fixed; top: 0; left: 0; right: 0; bottom: 0; background: #fff; z-index: 9999;"></div>
+		<div id="loader" style="position: fixed; top: 0; left: 0; right: 0; bottom: 0; background: #fff; z-index: 9999;"></div>
 		<div class="ui middle aligned center aligned grid">
 			<div class="column">
 				<h1>404</h1>
